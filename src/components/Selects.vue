63e49a38c5964ef40fe89df15f33273c09ae28bd
Antoniok2 <antoniopiazza18@gmail.com>
2021-11-29 14:30:54 +0100

    modified genre bar style

--- src/components/Selects.vue
+++ src/components/Selects.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="selection_genre">
       <select name="" id="">
           <option value="">All</option>
           <option value="Rock">Rock</option>
@@ -21,5 +21,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+    .selection_genre {
+        text-align: center;
+        margin: 10px;
 
+        select {
+        padding: 5px 20px;
+        border-radius: 20px;
+        color: white;
+
+        option {
+            color: white;
+        }
+        }
+    }
+    
 </style>
