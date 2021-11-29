bf0d503cb264604d1873d80ef5801b29cab29d24
Antoniok2 <antoniopiazza18@gmail.com>
2021-11-29 14:34:10 +0100

    Modified name export

--- src/components/Bonus.vue
+++ src/components/Bonus.vue
@@ -7,7 +7,7 @@
 
 <script>
 export default {
-  name: 'Cover'
+  name: 'Bonus'
 
 }
 </script>
--- src/components/Selects.vue
+++ src/components/Selects.vue
@@ -12,7 +12,7 @@
 
 <script>
 export default {
-  name: 'Cover',
+  name: 'Selects',
   props: {
       details: Object
   }
